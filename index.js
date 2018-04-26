1d5fe39bdbda7f77c639780ae3b8b088eb357ec1
Sahar Hathiramani <shathira@purdue.edu>
2018-04-26 16:02:11 -0400

    Updated to Include Sign-In Authentication

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function signinToCreate() {
 	document.getElementById("title").innerHTML = "Create Account";
 	document.getElementById("sign").setAttribute("hidden", "hidden");
 	document.getElementById("create").removeAttribute("hidden");
-	
+
 	document.getElementById("signInUsername").setAttribute("placeholder", "Email");
 	document.getElementById("signInPassword").setAttribute("placeholder", "Password");
 }
@@ -20,20 +20,33 @@ function signIn() {
 	var ret = true;
 	var usernameFlag = document.getElementById("signInUsername");
 	var passwordFlag = document.getElementById("signInPassword");
-	if(usernameFlag.value.length == 0) {
+	if (usernameFlag.value.length == 0) {
 		document.getElementById("signInUsername").setAttribute("placeholder", "Enter Valid Username");
 		ret = false;
 	}
-	
-	if(passwordFlag.value.length == 0) {
+
+	if (passwordFlag.value.length == 0) {
 		document.getElementById("signInPassword").setAttribute("placeholder", "Enter Valid Password");
 		ret = false;
 	}
-	
-	if(ret != false) {
-		window.location.href="../main.html"
-	}
-	
+
+	var email = usernameFlag.value;
+	var password = passwordFlag.value;
+
+	console.log(email);
+	console.log(password);
+
+	firebase.auth().signInWithEmailAndPassword(email, password).then(function (result) {
+		console.log(result);
+		window.location.href = '../main.html';
+	}).catch(function (error) {
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		console.log(errorCode);
+		console.log(errorMessage);
+		console.log("Error");
+		return;
+	});
 }
 
 function createAccount() {
@@ -41,24 +54,30 @@ function createAccount() {
 	var nameFlage = document.getElementById("createName");
 	var usernameFlag = document.getElementById("createUsername");
 	var passwordFlag = document.getElementById("createPassword");
-	
-	if(nameFlage.value.length == 0) {
+
+	if (nameFlage.value.length == 0) {
 		document.getElementById("createName").setAttribute("placeholder", "Enter Valid Name");
 		ret = false;
 	}
-	
-	if(usernameFlag.value.length == 0) {
+
+	if (usernameFlag.value.length == 0) {
 		document.getElementById("createUsername").setAttribute("placeholder", "Enter Valid Username");
 		ret = false;
 	}
-	
-	if(passwordFlag.value.length == 0) {
+
+	if (passwordFlag.value.length == 0) {
 		document.getElementById("createPassword").setAttribute("placeholder", "Enter Valid Password");
 		ret = false;
 	}
-	
-	if(ret != false) {
-		window.location.href="../main.html"
+
+	if (ret != false) {
+		window.location.href = "../main.html"
 	}
-	
 }
+
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+	// Handle Errors here.
+	var errorCode = error.code;
+	var errorMessage = error.message;
+	// ...
+});
